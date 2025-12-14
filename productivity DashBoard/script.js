f7e2a688044fc8c4025f7a74f597ac2de7028522
mohd zaid malik <mzaidmalik25@gmail.com>
2025-12-14 19:51:12 +0530

    Update productivity dashboard: layout & scripts

--- productivity DashBoard/script.js
+++ productivity DashBoard/script.js
@@ -1,12 +1,20 @@
 function openPage() {
  const allElem = document.querySelectorAll(".elem")
  const fullElem = document.querySelectorAll(".fullElem")
+ const closeBtn = document.querySelectorAll(".close")
 
  allElem.forEach((elem) => {
     elem.addEventListener("click", () => {
        fullElem[elem.id].style.display = "block"
     }) 
  })
+
+ closeBtn.forEach((btn) => {
+    btn.addEventListener("click", () => {
+        fullElem[btn.id].style.display = "none"
+    })
+ })
 }
 
 openPage()
+

2bd7ca8147f538bdc914f0dd64cc7b0146f8ee0e
mohd zaid malik <mzaidmalik25@gmail.com>
2025-12-16 15:33:55 +0530

    completed pomodore timer with functionalities

--- productivity DashBoard/script.js
+++ productivity DashBoard/script.js
@@ -1,97 +1,94 @@
 function openPage() {
- const allElem = document.querySelectorAll(".elem")
- const fullElem = document.querySelectorAll(".fullElem")
- const closeBtn = document.querySelectorAll(".close")
+  const allElem = document.querySelectorAll(".elem");
+  const fullElem = document.querySelectorAll(".fullElem");
+  const closeBtn = document.querySelectorAll(".close");
 
- allElem.forEach((elem) => {
+  allElem.forEach((elem) => {
     elem.addEventListener("click", () => {
-       fullElem[elem.id].style.display = "block"
-    }) 
- })
+      fullElem[elem.id].style.display = "block";
+    });
+  });
 
- closeBtn.forEach((btn) => {
+  closeBtn.forEach((btn) => {
     btn.addEventListener("click", () => {
-        fullElem[btn.id].style.display = "none"
-    })
- })
+      fullElem[btn.id].style.display = "none";
+    });
+  });
 }
-// openPage()
+openPage()
 
 function todoList() {
-let currentTask = []    
+  let currentTask = [];
 
-if(localStorage.getItem("currentTask")){
-    currentTask = JSON.parse(localStorage.getItem('currentTask'))
-} 
-else {
-    console.log("current Task is empty")
-}   
+  if (localStorage.getItem("currentTask")) {
+    currentTask = JSON.parse(localStorage.getItem("currentTask"));
+  } else {
+    console.log("current Task is empty");
+  }
 
-const renderTask = () => {
-    const allTask = document.querySelector(".rightBox")
-    let sum = ""
+  const renderTask = () => {
+    const allTask = document.querySelector(".rightBox");
+    let sum = "";
 
-    currentTask.forEach((task,idx) => {
-        sum += `<div class="w-full h-[70px] bg-[var(--pri)] flex items-center justify-between px-5 rounded-lg mb-2">
+    currentTask.forEach((task, idx) => {
+      sum += `<div class="w-full h-[70px] bg-[var(--pri)] flex items-center justify-between px-5 rounded-lg mb-2">
                    <h2 class="text-2xl font-[700] text-[var(--sec)]">${task.task}</h2>
                    <button id=${idx} class="remove px-5 py-3 bg-[var(--green)] text-white rounded-md text-xl font-[700] active:scale-95 transition-all duration-150">Mark as Done</button>
-                </div>`
-    })
-    allTask.innerHTML = sum
+                </div>`;
+    });
+    allTask.innerHTML = sum;
 
-    const removeTodo = document.querySelectorAll(".remove")
+    const removeTodo = document.querySelectorAll(".remove");
     removeTodo.forEach((btn) => {
-    btn.addEventListener("click", () => {
-    currentTask.splice(btn.id, 1)
-    localStorage.setItem("currentTask", JSON.stringify(currentTask))
-    renderTask()
-   })
-})
+      btn.addEventListener("click", () => {
+        currentTask.splice(btn.id, 1);
+        localStorage.setItem("currentTask", JSON.stringify(currentTask));
+        renderTask();
+      });
+    });
+  };
+
+  renderTask();
+
+  const todoForm = document.querySelector(".todo-form");
+  const todoInput = document.querySelector(".todo-input");
+  const todoTextArea = document.querySelector(".todo-textarea");
+
+  todoForm.addEventListener("submit", (e) => {
+    e.preventDefault();
+    if (todoInput.value == "" && todoTextArea.value == "") return;
+    currentTask.push({
+      task: todoInput.value,
+      textArea: todoTextArea.value,
+    });
+    renderTask();
+    localStorage.setItem("currentTask", JSON.stringify(currentTask));
+    todoInput.value = "";
+    todoTextArea.value = "";
+  });
 }
-
-renderTask()
-
-const todoForm = document.querySelector(".todo-form")
-const todoInput = document.querySelector(".todo-input")
-const todoTextArea = document.querySelector(".todo-textarea")
-
-todoForm.addEventListener("submit", (e) => {
-e.preventDefault()
-if(todoInput.value == "" && todoTextArea.value == "") return
-currentTask.push({
-    task: todoInput.value,
-    textArea: todoTextArea.value
-})
-renderTask()
-localStorage.setItem("currentTask", JSON.stringify(currentTask))
-todoInput.value = ""
-todoTextArea.value = ""
-})
-
-
-}
-// todoList()
+todoList()
 
 function dailyPlanner() {
-  const planner = document.querySelector(".day-planner")
+  const planner = document.querySelector(".day-planner");
 
-  const today = new Date().toDateString()
-  const savedDate = localStorage.getItem("plannerDate")
+  const today = new Date().toDateString();
+  const savedDate = localStorage.getItem("plannerDate");
 
   if (savedDate !== today) {
-    localStorage.removeItem("plannerObj")
-    localStorage.setItem("plannerDate", today)
+    localStorage.removeItem("plannerObj");
+    localStorage.setItem("plannerDate", today);
   }
 
   const hours = Array.from({ length: 18 }, (_, idx) => {
-    return `${6 + idx}:00 - ${7 + idx}:00`
-  })
+    return `${6 + idx}:00 - ${7 + idx}:00`;
+  });
 
-  let sum = ""
-  let plannerObj = JSON.parse(localStorage.getItem("plannerObj")) || {}
+  let sum = "";
+  let plannerObj = JSON.parse(localStorage.getItem("plannerObj")) || {};
 
   hours.forEach((time, idx) => {
-    let savedData = plannerObj[idx] || ""
+    let savedData = plannerObj[idx] || "";
     sum += `
       <div class="w-[49%] relative">
         <p class="absolute left-2 top-1">${time}</p>
@@ -101,36 +98,79 @@ function dailyPlanner() {
           type="text" 
           value="${savedData}">
       </div>
-    `
-  })
+    `;
+  });
 
-  planner.innerHTML = sum
+  planner.innerHTML = sum;
 
-  const plannerInput = document.querySelectorAll(".day-planner input")
+  const plannerInput = document.querySelectorAll(".day-planner input");
   plannerInput.forEach((elem) => {
     elem.addEventListener("input", () => {
-      plannerObj[elem.id] = elem.value
-      localStorage.setItem("plannerObj", JSON.stringify(plannerObj))
-    })
-  })
+      plannerObj[elem.id] = elem.value;
+      localStorage.setItem("plannerObj", JSON.stringify(plannerObj));
+    });
+  });
 }
 
-// dailyPlanner()
+dailyPlanner()
 
 function motivationQutote() {
+  const fetchQuote = async () => {
+    const res = await fetch("https://api.quotable.io/random");
+    const data = await res.json();
+    console.log(data);
+  };
 
-    const fetchQuote = async () => {
-      const res = await fetch("https://api.quotable.io/random")
-      const data = await res.json()
-      console.log(data)
-    }
-
-  fetchQuote()
+  fetchQuote();
 }
 
-// motivationQutote()
+motivationQutote()
 
-function Timer() {
+function timer() {
+  const stBtn = document.querySelector(".start-btn");
+  const pauseBtn = document.querySelector(".pause-btn");
+  const resetBtn = document.querySelector(".reset-btn");
+  const timer = document.querySelector(".timer");
 
+  let totalSec = 25 * 60;
+  let timerInterval = null;
+
+  function updateTimer() {
+    const min = Math.floor(totalSec / 60);
+    const sec = Math.floor(totalSec % 60);
+
+    timer.innerHTML = `${String(min).padStart("2", "0")} : ${String(
+      sec
+    ).padStart("2", "0")}`;
+  }
+
+  function start() {
+    clearInterval(timerInterval);
+    timerInterval = setInterval(() => {
+      if (totalSec > 0) {
+        totalSec--;
+        updateTimer();
+      } else {
+        clearInterval(timerInterval);
+      }
+    }, 1000);
+  }
+
+  function pause() {
+    clearInterval(timerInterval);
+  }
+
+  function reset() {
+    totalSec = 25 * 60;
+    clearInterval(timerInterval);
+    updateTimer();
+  }
+
+  stBtn.addEventListener("click", start);
+  pauseBtn.addEventListener("click", pause);
+  resetBtn.addEventListener("click", reset);
+
+  updateTimer();
 }
-Timer()
+
+timer()

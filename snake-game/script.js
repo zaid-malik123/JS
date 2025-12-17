0573c1d2fb8d89b3483ef716bbe35c2d329cc5f2
mohd zaid malik <mzaidmalik25@gmail.com>
2025-12-17 22:14:45 +0530

    snake game completed

--- snake-game/script.js
+++ snake-game/script.js
@@ -1,104 +1,184 @@
-const board = document.querySelector(".board")
+const board = document.querySelector(".board");
+const stModal = document.querySelector(".st-modal");
+const reStModal = document.querySelector(".restart-modal");
+const stBtn = document.querySelector(".st-btn");
+const reStBtn = document.querySelector(".restart-btn");
+const scoreEl = document.querySelector(".score");
+const highScoreEl = document.querySelector(".high-score");
+const timeEl = document.querySelector(".time");
 
-const blockHeight = 40
-const blockWidth = 40
+let score = 0;
+let seconds = 0;
+let timeInterval = null;
 
-const rows = Math.floor(board.clientHeight / blockHeight)
-const cols = Math.floor(board.clientWidth / blockWidth)
-const dimensions = []
-let direction = "down"
+// localStorage se high score lao
+let highScore = localStorage.getItem("snakeHighScore") || 0;
+highScoreEl.innerText = highScore;
+
+function startTimer() {
+  clearInterval(timeInterval);
+  seconds = 0;
+
+  timeInterval = setInterval(() => {
+    seconds++;
+
+    const min = String(Math.floor(seconds / 60)).padStart(2, "0");
+    const sec = String(seconds % 60).padStart(2, "0");
+
+    timeEl.innerText = `${min} : ${sec}`;
+  }, 1000);
+}
+
+function stopTimer() {
+  clearInterval(timeInterval);
+}
+
+
+const blockHeight = 40;
+const blockWidth = 40;
+
+const rows = Math.floor(board.clientHeight / blockHeight);
+const cols = Math.floor(board.clientWidth / blockWidth);
+const dimensions = [];
+let direction = "down";
 let inerval = null;
-let food = {x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)}
-console.log(food)
+let food = {
+  x: Math.floor(Math.random() * rows),
+  y: Math.floor(Math.random() * cols),
+};
+console.log(food);
 
-const snake = [
+let snake = [
   {
-    x: 1, y: 3,
-  }, 
-  {
-    x: 1, y: 4,
+    x: 1,
+    y: 3,
   },
-  {
-    x: 1, y: 5,
-  }
-]
+];
 
 for (let i = 0; i < rows; i++) {
   for (let j = 0; j < cols; j++) {
-    const block = document.createElement("div")
+    const block = document.createElement("div");
 
     block.classList.add(
       "h-[40px]",
       "w-[40px]",
       "border",
       "border-gray-800",
-      "text-white",         
+      "text-white",
       "flex",
       "items-center",
       "justify-center"
-    )
-    board.appendChild(block)
+    );
+    board.appendChild(block);
     // block.innerText = `${i}-${j}`
-    dimensions[`${i}-${j}`] = block
+    dimensions[`${i}-${j}`] = block;
     // dimensions.push(${i}-${j})
   }
 }
 
-function renderSnake(){
+function renderSnake() {
   let head = null;
-  
-  dimensions[`${food.x}-${food.y}`].style.backgroundColor = "pink"
-  
-  if(direction == "right"){
-    head = {x: snake[0].x, y: snake[0]. y + 1}
+
+  dimensions[`${food.x}-${food.y}`].style.backgroundColor = "pink";
+
+  if (direction == "right") {
+    head = { x: snake[0].x, y: snake[0].y + 1 };
   }
-  if(direction == "left"){
-    head = {x: snake[0].x, y: snake[0]. y - 1}
-  }
-  
-  if(direction == "up"){
-    head = {x: snake[0]. x - 1, y: snake[0]. y}
+  if (direction == "left") {
+    head = { x: snake[0].x, y: snake[0].y - 1 };
   }
 
-  if(direction == "down"){
-    head = {x: snake[0]. x + 1, y: snake[0]. y}
+  if (direction == "up") {
+    head = { x: snake[0].x - 1, y: snake[0].y };
   }
-  if(head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols){
-    alert("Game Over")
-    clearInterval(inerval)
-  }
-  if(head.x == food.x && head.y == food.y){
-    dimensions[`${food.x}-${food.y}`].style.backgroundColor = ""
-    food = {x: Math.floor(Math.random() * rows), y: Math.floor(Math.random() * cols)}
-    snake.unshift(head)
-  }
-  snake.forEach(segment => {
-    dimensions[`${segment.x}-${segment.y}`].style.backgroundColor = ""
-  }) 
-  snake.unshift(head)
-  snake.pop()
-   
 
-  snake.forEach(segment => {
-    dimensions[`${segment.x}-${segment.y}`].style.backgroundColor = "white"
-  })
+  if (direction == "down") {
+    head = { x: snake[0].x + 1, y: snake[0].y };
+  }
+  if (head.x < 0 || head.x >= rows || head.y < 0 || head.y >= cols) {
+    reStModal.style.display = "flex";
+    clearInterval(inerval);
+    stopTimer()
+    if (score > highScore) {
+    highScore = score;
+    localStorage.setItem("snakeHighScore", highScore);
+    highScoreEl.innerText = highScore;
+  }
+  return
+  }
+  if (head.x == food.x && head.y == food.y) {
+
+    dimensions[`${food.x}-${food.y}`].style.backgroundColor = "";
+    food = {
+      x: Math.floor(Math.random() * rows),
+      y: Math.floor(Math.random() * cols),
+    };
+    snake.unshift(head);
+    score++;
+    scoreEl.innerText = score;
+  }
+  snake.forEach((segment) => {
+    dimensions[`${segment.x}-${segment.y}`].style.backgroundColor = "";
+  });
+  snake.unshift(head);
+  snake.pop();
+
+  snake.forEach((segment) => {
+    dimensions[`${segment.x}-${segment.y}`].style.backgroundColor = "white";
+  });
 }
 
-inerval = setInterval(() => {
-  renderSnake()
-}, 300)
+stBtn.addEventListener("click", () => {
+  score = 0;
+  scoreEl.innerText = score;
+  startTimer()
+  inerval = setInterval(() => {
+    renderSnake();
+  }, 300);
+
+  stModal.style.display = "none";
+  //  stModal.style.display = "flex"
+});
+
+reStBtn.addEventListener("click", () => {
+  clearInterval(inerval);
+  stopTimer();
+
+  score = 0;
+  scoreEl.innerText = score;
+  dimensions[`${food.x}-${food.y}`].style.backgroundColor = "";
+  snake.forEach((segment) => {
+    dimensions[`${segment.x}-${segment.y}`].style.backgroundColor = "";
+  });
+  reStModal.style.display = "none";
+  snake = [
+    {
+      x: 1,
+      y: 3,
+    },
+  ];
+  direction = "down"
+  food = {
+  x: Math.floor(Math.random() * rows),
+  y: Math.floor(Math.random() * cols),
+  };
+  startTimer()
+  inerval = setInterval(() => {
+    renderSnake();
+  }, 300)
+});
 
 addEventListener("keydown", (e) => {
-  if(e.key == "ArrowUp"){
-    direction = "up"
+  if (e.key == "ArrowUp") {
+    direction = "up";
   }
-  if(e.key == "ArrowDown"){
-    direction = "down"
+  if (e.key == "ArrowDown") {
+    direction = "down";
   }
-  if(e.key == "ArrowLeft"){
-    direction = "left"
+  if (e.key == "ArrowLeft") {
+    direction = "left";
   }
-  if(e.key == "ArrowRight"){
-    direction = "right"
+  if (e.key == "ArrowRight") {
+    direction = "right";
   }
-})
+});

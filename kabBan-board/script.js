c56d10befdaf2024750b0f6622a00cfa86a1a635
mohd zaid malik <mzaidmalik25@gmail.com>
2025-12-18 14:05:29 +0530

    drag and drop feature done

---
+++ kabBan-board/script.js
@@ -0,0 +1,53 @@
+const todoColumn = document.querySelector("#todoCol");
+const progressColumn = document.querySelector("#progressCol");
+const doneColumn = document.querySelector("#doneCol");
+const tasks = document.querySelectorAll(".task");
+
+let dragItem = null;
+tasks.forEach((task) => {
+  task.addEventListener("drag", (e) => {
+    dragItem = task;
+  });
+});
+
+function dragEventsCol(col) {
+  col.addEventListener("dragenter", (e) => {
+    e.preventDefault();
+    col.classList.add(
+      "scale-102",
+      "border",
+      "border-dashed",
+      "border-blue-400"
+    );
+  });
+
+  col.addEventListener("dragleave", (e) => {
+    e.preventDefault();
+    col.classList.remove(
+      "scale-102",
+      "border",
+      "border-dashed",
+      "border-blue-400"
+    );
+  });
+
+  col.addEventListener("dragover", (e) => {
+    e.preventDefault();
+  });
+
+  col.addEventListener("drop", (e) => {
+    // console.log("Dropped", e)
+    // console.log(dragItem, col)
+    col.appendChild(dragItem);
+    col.classList.remove(
+      "scale-102",
+      "border",
+      "border-dashed",
+      "border-blue-400"
+    );
+  });
+}
+
+dragEventsCol(todoColumn);
+dragEventsCol(progressColumn);
+dragEventsCol(doneColumn);

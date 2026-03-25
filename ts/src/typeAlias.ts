3f71596ec85eddb9672a9a272250d1b32f282fd1
Zaid Malik <mzaidmalik25@gmail.com>
2026-03-25 13:41:06 +0530

    learning type aliases

---
+++ ts/src/typeAlias.ts
@@ -0,0 +1,41 @@
+// type Pet = {
+//     name: string,
+//     age: number,
+//     type: "dog" | "cat"
+// }
+
+// // const pet: Pet = {
+// //     name: "tommy",
+// //     age: 12,
+// //     type: "cat"
+// // }
+
+// type traffic = "red" | "green" | "blue";
+
+// const light: traffic = "blue"
+
+// type Bird = {
+//     name: string,
+//     canFly: boolean
+// }
+
+// type CheckFly = (bird: Bird) => boolean;
+
+// function isFly(bird: Bird): boolean {
+//     if(bird.canFly) {
+//         return true;
+//     } else {
+//         return false;
+//     }
+// }
+
+// function isFlyShort(bird: Bird): boolean {
+//     return bird.canFly;
+// }
+
+// const parrot: Bird = { name: "Parrot", canFly: true };
+// const penguin: Bird = { name: "Penguin", canFly: false };
+
+// console.log(isFly(parrot));   // true
+// console.log(isFly(penguin));  // false
+// console.log(isFlyShort(parrot)); // true
